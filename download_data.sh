f7c0b2cdceb26bb32f02443d2040777822b74582
zelunli <zelun.li9@gmail.com>
2025-11-13 18:14:55 +0800

    faster download

--- download_data.sh
+++ download_data.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Trained Sei model
-wget https://zenodo.org/record/4906997/files/sei_model.tar.gz
+aria2c -x 16 -s 16 -k 1M \
+  -o sei_model.tar.gz \
+  "https://zenodo.org/record/4906997/files/sei_model.tar.gz"
 
 tar -xzvf sei_model.tar.gz
 
